a0d0ea53650ecc6b242a626ca6098514ea6353cf
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2023-07-24 18:10:34 +0100

    Add files via upload

---
+++ src/me/Josh123likeme/Render3D3p0/Main.java
@@ -0,0 +1,15 @@
+package me.Josh123likeme.Render3D3p0;
+
+import java.util.Random;
+
+public class Main {
+
+	public static Random random = new Random();
+	
+	public static void main(String[] args) {
+		
+		new Game();
+		
+	}
+	
+}
